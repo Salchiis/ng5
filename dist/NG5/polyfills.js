a75e91a8cafa3463b8996d3d685c01391b90d9b2
Weebmaster666 <A0173269@itesm.mx>
2021-12-01 17:38:56 -0600

    more css changes

--- dist/NG5/polyfills.js
+++ dist/NG5/polyfills.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Usuario\Desktop\Angular\NG5\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\puma0\Desktop\ng5\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
--- dist/NG5/styles.js
+++ dist/NG5/styles.js
@@ -440,7 +440,7 @@ module.exports = content.locals || {};
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Usuario\Desktop\Angular\NG5\src\styles.scss */"./src/styles.scss");
+module.exports = __webpack_require__(/*! C:\Users\puma0\Desktop\ng5\src\styles.scss */"./src/styles.scss");
 
 
 /***/ })
