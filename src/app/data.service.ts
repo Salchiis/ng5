ccb9b29d13c0aaa31e2716468aff2f6a7eed779d
Salchiis <d1360_audi@hotmail.com>
2021-12-03 08:06:36 -0600

    Front2

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,27 +7,13 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class DataService {
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
-  getAll(): Observable<any> {
-    return this.http.get(url, description, );
-  }
-
-  get(id): Observable<any> {
-    return this.http.get(`${baseUrl}/${id}`);
-  }
-
-  create(data): Observable<any> {
-    return this.http.post(baseUrl, data);
-  }
-
-  update(id, data): Observable<any> {
-    return this.http.put(`${baseUrl}/${id}`, data);
-  }
+ 
 
   private goals = new BehaviorSubject<any>(['first goal', 'other goal']);
   goal = this.goals.asObservable;
-  constructor() { }
+ 
 
   changeGoal(goal){
     this.goals.next(goal);
